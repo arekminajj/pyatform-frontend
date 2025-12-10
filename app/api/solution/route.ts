11209cfc33e6c657f893706560727fc6ac0bd37c
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-12-10 21:48:03 +0100

    feat: #main add solution api endpoint

---
+++ app/api/solution/route.ts
@@ -0,0 +1,33 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+import type { NextRequest } from "next/server";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.accessToken) {
+    redirect("/api/auth/signin");
+  }
+
+  const body = await req.json();
+  const res = await fetch(process.env.BACKEND_BASE_URL + "/api/solution", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + session.accessToken,
+    },
+    body: JSON.stringify(body),
+    method: "POST",
+  });
+
+  if (res.status >= 200 && res.status <= 300) {
+    return Response.json({
+      status: res.status,
+      message: "Solution added",
+    });
+  }
+
+  const data = await res.json();
+  return Response.json({ data }, { status: 400 });
+}
